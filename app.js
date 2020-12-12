fe0c4e7d471e83ed21a5606c6cf3d34ae3888a94
kishoreBandanadam <krrish.kishore02@gmail.com>
2020-12-12 19:52:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const pdf = require("html-pdf");
+const port = process.env.PORT || 3000;
+
+let students = [
+    {name: "Joy",
+     email: "joy@example.com",
+     city: "New York",
+     country: "USA"},
+    {name: "John",
+     email: "John@example.com",
+     city: "San Francisco",
+     country: "USA"},
+    {name: "Clark",
+     email: "Clark@example.com",
+     city: "Seattle",
+     country: "USA"},
+    {name: "Watson",
+     email: "Watson@example.com",
+     city: "Boston",
+     country: "USA"},
+    {name: "Tony",
+     email: "Tony@example.com",
+     city: "Los Angels",
+     country: "USA"
+ }];
+
+ let users = ['geddy', 'neil', 'alex'];
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('pages/home', {
+        users : users,
+        title : "Ejs Home"
+	});
+});
+
+app.get('/home', (req, res) => {
+	res.render('pages/home', {
+        users : users,
+        title : "Ejs Home"
+	});
+});
+
+app.get('/about', (req, res) => {
+    console.log('__dirname', __dirname);
+	res.render('pages/about', {
+        users : users,
+        title : "Ejs About"
+	});
+});
+
+app.get('/api/generate-pdf', (req, res) => {
+    // console.log('__dirname', __dirname);
+    // console.log('path', path.join(__dirname, './views/', 'pages/report-template.ejs'));
+    ejs.renderFile(path.join(__dirname, './views/', 'pages/report-template.ejs'), {
+        students: students
+	}, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            //res.send('pages/home');
+            let options = {
+                "height": "11.25in",
+                "width": "8.5in",
+                "header": {
+                    "height": "20mm"
+                },
+                "footer": {
+                    "height": "20mm",
+                },
+            };
+            pdf.create(data, options).toFile("report.pdf", function (err, data) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("File created successfully");
+                }
+            });
+        }
+        
+    });
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
